67e32c7699aab585a3da8d5e488b0bba2c8ad8c0
Thomas Dall'Agnese <thomas@dallagnese.fr>
2011-06-15 18:33:12 +0900

    record ambu position over time

--- draft.py
+++ draft.py
@@ -1,20 +1,101 @@
-#!/usr/bin/env python
-from numpy.lib import *
-import Gnuplot
-from math import sin, pi
+##!/usr/bin/env python
+#from numpy.lib import *
+#import Gnuplot
+#from math import sin, pi
+#
+#x = linspace(0,10,31)
+#y = x**2
+#y2 = 10*sin(pi*x)
+#g = Gnuplot.Gnuplot() #!! Won't be able to use 'with' in python 2.6?
+#d = Gnuplot.Data(x,y,title='squared')
+#d2=Gnuplot.Data(x,y2,title='sine')
+#g('set grid')
+#g.xlabel('x axis')
+#g.ylabel('y axis')
+#g.plot(d,d2)
+#ans = raw_input('Enter f to create .png file, or Enter to quit ')
+#if ans == 'f':
+#    g.hardcopy('filename.png',terminal = 'png')
+#    #g.hardcopy('filename.ps',terminal='postscript',enhanced=1,color=1)
+#g.reset()
 
-x = linspace(0,10,31)
-y = x**2
-y2 = 10*sin(pi*x)
-g = Gnuplot.Gnuplot() #!! Won't be able to use 'with' in python 2.6?
-d = Gnuplot.Data(x,y,title='squared')
-d2=Gnuplot.Data(x,y2,title='sine')
-g('set grid')
-g.xlabel('x axis')
-g.ylabel('y axis')
-g.plot(d,d2)
-ans = raw_input('Enter f to create .png file, or Enter to quit ')
-if ans == 'f':
-    g.hardcopy('filename.png',terminal = 'png')
-    #g.hardcopy('filename.ps',terminal='postscript',enhanced=1,color=1)
-g.reset()
+
+#import Gnuplot
+#g = Gnuplot.Gnuplot()
+#x = [1000,1001,1002,1003]
+#y = [1,2,3,4]
+#d = Gnuplot.Data(x,y,title='prout')
+#g.plot(d)
+#g.hardcopy('filename.png',terminal = 'svg')
+
+
+import os
+import Gnuplot
+
+outputFolder = '/home/thomas/Dropbox/Keio/research/results'
+if os.path.exists(outputFolder):
+    for path, subdirs, files in os.walk(outputFolder):
+        for name in filter(lambda x: x.endswith('_ambu.txt'), files):
+            filePath = os.path.join(path,name)
+            basename, ext = os.path.splitext(name)
+            with open(filePath) as ambuResult:
+                currentData = {'time':[],'pos':[],'vel':[],'velms':[]}
+                currentAccData = {'time':[],'acc':[]}
+                k   = 0
+                mod = 10
+                previousPos     = -1
+                previousTime    = -1
+                previousVel     = -1
+                previousVelMS   = -1
+                previousVelSum  = -1
+                previousTimeSum = -1
+                currentVelSum   = 0
+                for line in ambuResult:
+                    r =line.replace('\n','').split(' ')
+                    currentTime = float(r[0])-1000.0
+                    currentPos  = float(r[1])
+                    if previousPos>-1:
+                        # calculate velocity
+                        currentVelMS = (currentPos-previousPos)/(currentTime-previousTime)
+                        currentVel = currentVelMS*3600.0/1000.0
+                        currentVelSum += currentVelMS
+                        if previousVel>-1:
+                            #acc = (currentVelMS-previousVelMS)/(currentTime-previousTime)
+                            currentData['time'].append(currentTime)
+                            currentData['pos'].append(currentPos)
+                            currentData['vel'].append(currentVel)
+                            currentData['velms'].append(currentVelMS)
+                            k = (k+1) % mod
+                            if k==0:
+                                if previousVelSum>-1:
+                                    acc = (currentVelSum-previousVelSum)/(mod*(currentTime-previousTimeSum))
+                                    currentAccData['acc'].append(acc)
+                                    currentAccData['time'].append(currentTime)
+                                previousVelSum = currentVelSum
+                                previousTimeSum = currentTime
+                                currentVelSum = 0
+                        previousVel = currentVel
+                        previousVelMS = currentVelMS
+                    previousTime = currentTime
+                    previousPos  = currentPos
+            print currentData
+            print currentAccData
+            g = Gnuplot.Gnuplot()
+            x = currentData['time']
+            v = currentData['vel']
+            a = currentAccData['acc']
+            d1 = Gnuplot.Data(x,v,title='velocity (km/h)',smooth='csplines with lines')
+            #d1 = Gnuplot.Data(x,v,title='velocity (km/h)')
+            #d2 = Gnuplot.Data(currentAccData['time'],a,title='acceleration (m/s2)',smooth='csplines with lines')
+            d2 = Gnuplot.Data(currentAccData['time'],a,title='acceleration (m/s2)',smooth='freq with boxes')
+            # 'smooth csplines  with lines'
+            g.xlabel('Time (s)')
+            #g.ylabel('Velocity (km/h)')
+            g.title(name)
+            g.plot(d1)
+            g.hardcopy(basename+'_vel.svg',terminal='svg',enhanced=1,size='1024 768')
+            #g.ylabel('Acceleration (m/s2)')
+            #g('set style histogram')
+            g.plot(d2)
+            g.hardcopy(basename+'_acc.svg',terminal='svg',enhanced=1,size='1024 768')
+            exit(0)
--- wafThread.py
+++ wafThread.py
@@ -4,6 +4,7 @@ from PySide.QtCore import QObject, Signal, QRunnable
 import subprocess, threading
 import os, json, random
 from datetime import datetime
+import Gnuplot
 
 def dateToFilename(d=-1,rn=-1):
     if rn==-1:
@@ -46,6 +47,7 @@ class WafThread(QRunnable, QObject):
         QRunnable.__init__(self)
         QObject.__init__(self)
         self.options = ''
+        self.optionsDict = options
         for option in options:
             if option=='vel1' or option=='spl':
                 speedInMs = options[option].getValue()*1000/3600
@@ -67,7 +69,11 @@ class WafThread(QRunnable, QObject):
                 settings[option] = options[option].getValue()
         self.output = {'settings':settings,'command':'%s %s%s' % (self.command, ' --run \'', self.scenario+self.options+'\'')}
     def run(self, *args, **kwargs):
-        self.outputFile = os.path.join(self.outputFolder,dateToFilename(datetime.now(), self.runNumber)+'.txt')
+        outputBase = os.path.join(self.outputFolder,dateToFilename(datetime.now(), self.runNumber))
+        self.outputFile = outputBase+'.txt'
+        self.outputAmbu = outputBase+'_ambu.txt'
+        basename, ext = os.path.splitext(self.outputFile)
+        self.options += ' --ambu=1 --af=%s' % self.outputAmbu
         out = open(self.outputFile, 'w')
         command = Ns2Command(self.options, out)
         self.startTime = datetime.now()
@@ -82,8 +88,67 @@ class WafThread(QRunnable, QObject):
             #print result
             jsonResult = '{\n'+result+'"end":"True"\n}'
             try:
-                self.output['results'] = json.loads(jsonResult)
+                jsonR = json.loads(jsonResult)
+                self.output['results'] = jsonR
                 self.output['succeed'] = 1
+                # Plot Ambu speed/accel
+                with open(self.outputAmbu) as ambuResult:
+                    currentData = {'time':[],'pos':[],'vel':[],'velms':[]}
+                    currentAccData = {'time':[],'acc':[]}
+                    k   = 0
+                    mod = 10
+                    previousPos     = -1
+                    previousTime    = -1
+                    previousVel     = -1
+                    previousVelMS   = -1
+                    previousVelSum  = -1
+                    previousTimeSum = -1
+                    currentVelSum   = 0
+                    for line in ambuResult:
+                        r =line.replace('\n','').split(' ')
+                        currentTime = float(r[0])-1000.0
+                        currentPos  = float(r[1])
+                        if previousPos>-1:
+                            # calculate velocity
+                            currentVelMS = (currentPos-previousPos)/(currentTime-previousTime)
+                            currentVel = currentVelMS*3600.0/1000.0
+                            currentVelSum += currentVelMS
+                            if previousVel>-1:
+                                #acc = (currentVelMS-previousVelMS)/(currentTime-previousTime)
+                                currentData['time'].append(currentTime)
+                                currentData['pos'].append(currentPos)
+                                currentData['vel'].append(currentVel)
+                                currentData['velms'].append(currentVelMS)
+                                k = (k+1) % mod
+                                if k==0:
+                                    if previousVelSum>-1:
+                                        acc = (currentVelSum-previousVelSum)/(mod*(currentTime-previousTimeSum))
+                                        currentAccData['acc'].append(acc)
+                                        currentAccData['time'].append(currentTime)
+                                    previousVelSum = currentVelSum
+                                    previousTimeSum = currentTime
+                                    currentVelSum = 0
+                            previousVel = currentVel
+                            previousVelMS = currentVelMS
+                        previousTime = currentTime
+                        previousPos  = currentPos
+                ambuResult.close()
+                # Gnuplot
+                g = Gnuplot.Gnuplot()
+                x = currentData['time']
+                v = currentData['vel']
+                a = currentAccData['acc']
+                d1 = Gnuplot.Data(x,v,title='velocity (km/h)',smooth='csplines with lines')
+                d2 = Gnuplot.Data(currentAccData['time'],a,title='acceleration (m/s2)',smooth='freq with boxes')
+                # 'smooth csplines  with lines'
+                g.xlabel('Time (sec.)')
+                #g.ylabel('Velocity (km/h)')
+                g.title('[prate:%d][rn:%d][flow:%2.2f][gap:%d]' % (self.optionsDict['prate'],self.runNumber,self.optionsDict['flow1'],jsonR['averageGapOnLane0']))
+                g.plot(d1)
+                g.hardcopy(basename+'_vel.svg',terminal='svg',enhanced=1,size='1024 768')
+                #g.ylabel('Acceleration (m/s2)')
+                g.plot(d2)
+                g.hardcopy(basename+'_acc.svg',terminal='svg',enhanced=1,size='1024 768')
             except:
                 self.output['results'] = result
                 self.output['succeed'] = "Error decoding Json Result"
