f8564a5fc60fc1249e651219c28b572c86720378
Thomas Dall'Agnese <thomas@dallagnese.fr>
2011-06-21 10:26:14 +0900

    multiple scenarios

--- main.py
+++ main.py
@@ -5,11 +5,11 @@ from PySide.QtGui import QApplication
 from highwaySimulatorGui import *
 import sys
 
-def trace(frame, event, arg):
-    with open('/home/thomas/ZumoDrive/Highway_Simulation_GUI/trace.log','a') as log:
-        log.write("%s, %s:%d\n" % (event, frame.f_code.co_filename, frame.f_lineno))
-    return trace
-sys.settrace(trace)
+#def trace(frame, event, arg):
+#    with open('/home/thomas/ZumoDrive/Highway_Simulation_GUI/trace.log','a') as log:
+#        log.write("%s, %s:%d\n" % (event, frame.f_code.co_filename, frame.f_lineno))
+#    return trace
+#sys.settrace(trace)
 
 app = QApplication(sys.argv)
 app.setApplicationName("Highway Simulation GUI")
